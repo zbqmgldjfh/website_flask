a39a41e51bb5fde1f14c569684332d7b6032d88b
zbqmgldjfh <60593969+zbqmgldjfh@users.noreply.github.com>
2021-02-24 17:04:38 +0900

    글 확인목록
    확인창 구현

--- run.py
+++ run.py
@@ -3,10 +3,45 @@ from flask import request
 from flask import render_template
 from flask_pymongo import PyMongo
 from datetime import datetime
+from bson.objectid import ObjectId
+from flask import abort
+from flask import redirect
+from flask import url_for
+import time
 
 app = Flask(__name__)  #flask 인스턴스 생성
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myweb"
 mongo = PyMongo(app)
+
+@app.template_filter("formatdatetime")
+def format_datetime(value):
+    if value is None:
+        return ""
+
+    now_timestamp = time.time()
+    offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
+    value = datetime.fromtimestamp((int(value) / 1000)) + offset
+    return value.strftime("%Y-%m-%d %H:%M:%S")
+
+@app.route("/view/<idx>")
+def board_view(idx):
+    #idx = request.args.get("idx")
+    if idx is not None:
+        board = mongo.db.board
+        data = board.find_one({"_id": ObjectId(idx)})
+
+        if data is not None:
+            result = {
+                "id": data.get("_id"),
+                "name": data.get("name"),
+                "title": data.get("title"),
+                "contents": data.get("contents"),
+                "pubdata": data.get("pubdata"),
+                "view": data.get("view")
+            }
+
+            return render_template("view.html", result = result)
+    return abort(404)
 
 @app.route("/write", methods=["GET", "POST"])
 def board_write():
@@ -25,9 +60,9 @@ def board_write():
             "view": 0,
         }
 
-        board.insert_one(post)
-
-        return ""
+        x = board.insert_one(post)
+        print(x.inserted_id)
+        return redirect(url_for("board_view", idx = x.inserted_id))
     else:
         return render_template("write.html")
 
